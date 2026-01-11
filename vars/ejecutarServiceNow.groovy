79c6f7f55c50718972aafc9ce7d8360a2fa64a2b
UO300896 <UO300896@uniovi.es>
2026-01-11 01:19:59 +0100

    Update parameters to use empty default values

--- vars/ejecutarServiceNow.groovy
+++ vars/ejecutarServiceNow.groovy
@@ -1,11 +1,12 @@
 import ServiceNowClient
 def call() {
+    System.setProperty("file.encoding", "UTF-8")
     pipeline {
         agent any
         
         parameters {
-            string(name: 'STR_CAMBIO', defaultValue: 'CHG0000005', description: 'Número del Cambio')
-            string(name: 'STR_TAREA', defaultValue: 'CTASK0010003', description: 'Número de la Tarea')
+            string(name: 'STR_CAMBIO', defaultValue: '', description: 'Número del Cambio')
+            string(name: 'STR_TAREA', defaultValue: '', description: 'Número de la Tarea')
             text(name: 'STR_MENSAJE', defaultValue: 'Actualización desde Jenkins', description: 'Mensaje de la nota')
             booleanParam(name: 'BOOL_CERRAR', defaultValue: false, description: '¿Cerrar la tarea al finalizar?')
         }
@@ -24,7 +25,7 @@ def call() {
                         // 2. Si el usuario marcó el check, cerramos la tarea
                         if (params.BOOL_CERRAR) {
                             echo "Cerrando la tarea ${params.STR_TAREA}..."
-                            sn.cerrarTarea(params.STR_CAMBIO, params.STR_TAREA, "Cierre automático: ${params.STR_MENSAJE}")
+                            sn.cerrarTarea(params.STR_CAMBIO, params.STR_TAREA, "${params.STR_MENSAJE}")
                         }
                     }
                 }
