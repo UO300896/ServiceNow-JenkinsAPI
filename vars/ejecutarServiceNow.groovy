6b4a459c249447ffd59bca90cb56d682e4cffe67
UO300896 <UO300896@uniovi.es>
2026-01-13 03:22:26 +0100

    Add waiting task parameters to pipeline
    Added parameters for waiting task and message in the script.

--- vars/ejecutarServiceNow.groovy
+++ vars/ejecutarServiceNow.groovy
@@ -7,7 +7,9 @@ def call() {
         parameters {
             string(name: 'STR_CAMBIO', defaultValue: '', description: 'Número del Cambio')
             string(name: 'STR_TAREA', defaultValue: '', description: 'Número de la Tarea')
+            string(name: 'STR_TAREA_EN_ESPERA', defaultValue: '', description: 'Número de la Tarea')
             text(name: 'STR_MENSAJE', defaultValue: 'Actualización desde Jenkins', description: 'Mensaje de la nota')
+            text(name: 'STR_MENSAJE_EN_ESPERA', defaultValue: 'En espera a fecha de ejecución', description: 'Mensaje de la nota')
             booleanParam(name: 'BOOL_CERRAR', defaultValue: false, description: '¿Cerrar la tarea al finalizar?')
         }
 
@@ -22,10 +24,12 @@ def call() {
                         echo "Añadiendo nota a la tarea ${params.STR_TAREA}..."
                         sn.documentarNotaDeTarea(params.STR_CAMBIO, params.STR_TAREA, params.STR_MENSAJE)
 
-                        // 2. Si el usuario marcó el check, cerramos la tarea
+                        // 2. Si el usuario marcó el check, cerramos la tarea y ponemos la tarea siguiente en espera
                         if (params.BOOL_CERRAR) {
                             echo "Cerrando la tarea ${params.STR_TAREA}..."
                             sn.cerrarTarea(params.STR_CAMBIO, params.STR_TAREA, "${params.STR_MENSAJE}")
+                            echo "Poniendo tarea en espera ${params.TAREA_EN_ESPERA}..."
+                            sn.cerrarTarea(params.STR_CAMBIO, params.STR_TAREA_EN_ESPERA, "${params.STR_MENSAJE_EN_ESPERA}")
                         }
                         
                         // 3. Siempre documentamos la nota del cambio (usando los parámetros del usuario)
